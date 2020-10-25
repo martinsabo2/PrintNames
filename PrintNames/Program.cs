2786e5f116b70bc556b4512db903f20934ac3c27
martin.sabo <martin.sabo@itvkurze.sk>
2020-10-25 16:40:20 +0100

    Add project files.

---
+++ PrintNames/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrintNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Martin Sabo");
+            // Console.WriteLine("dalsie meno");
+        }
+    }
+}
